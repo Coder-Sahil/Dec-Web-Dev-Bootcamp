4f9c4292f531115ff9bf27dab4e32791e41a875a
Coder-Sahil <1sahiltrivedi@gmail.com>
2021-12-19 19:32:21 +0530

    Putting API data into table format using literals

--- Module-4/main.js
+++ Module-4/main.js
@@ -11,22 +11,34 @@ const itemAPI =
 const requestBtn = document.getElementById("request-button");
 const tableResponse = document.getElementById("response");
 
-const beautifier = () => {
-  const obj = {
-    name: "Pen",
-    id: 1,
-    price: 10,
-    rating: 5,
-  };
+const beautifier = (data) => {
+  // const obj = {
+  //   name: "Pen",
+  //   id: 1,
+  //   price: 10,
+  //   rating: 5,
+  // };
 
-  const HTML_MARKUP = `
-    <tr>
-      <td>${obj.name}</td>
-      <td>${obj.id}</td>
-      <td>${obj.price}</td>
-      <td>${obj.rating}</td>
-    </tr>
-  `; // ES6
+  const obj = data;
+  const productList = obj.product;
+
+  const HTML_MARKUP = productList.map(
+    (product) =>
+    `.<tr>
+        <td>${product.name}</td>
+        <td>${product.id}</td>
+        <td>${product.price}</td>
+        <td>${product.rating}</td>
+      </tr>`);
+
+  // const HTML_MARKUP = `
+  //   <tr>
+  //     <td>${obj.name}</td>
+  //     <td>${obj.id}</td>
+  //     <td>${obj.price}</td>
+  //     <td>${obj.rating}</td>
+  //   </tr>
+  // `; // ES6
 
   return HTML_MARKUP;
 };
@@ -36,7 +48,7 @@ const requestItems = async () => {
   const response = await fetch(itemAPI); // XMLHttpRequest
   const data = await response.json();
 
-  tableResponse.innerHTML += beautifier();
+  tableResponse.innerHTML += beautifier(data);
 };
 
 requestBtn.addEventListener("click", requestItems);
